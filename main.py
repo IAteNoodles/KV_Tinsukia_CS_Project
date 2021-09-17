5116ae6cfef0d7d61050b68c1f9f64ac4e2968c1
Crippling-Anxiety <450iv7c71@relay.firefox.com>
2021-09-17 22:04:04 +0530

    Starting the project, Banking System

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ import sys
 
 
 class Bank:
-    def __init__(self, customer_name, account_type):
+    def __init__(self, customer_name: str, account_type: str):
         self.CUSTOMER_NAME = customer_name
         self.ACCOUNT_TYPE = account_type
         self.__IFSC__ = "000LIGN"
@@ -16,7 +16,7 @@ class Bank:
         self.ACCOUNT_NUMBER = account_number
 
 
-def accept_input():
+def accept_input() -> tuple:
     customer_name = input("Enter your name: ")
     print("Account Types Available:")
     print("1)Savings\n2)Current\n3)Corporate")
@@ -27,9 +27,20 @@ def accept_input():
         if not input("Press any key to retry and enter to end: ") is None:
             accept_input()
     account_type = "Savings" if account_type == 1 else "Current" if account_type == 2 else "Corporate"
-    a = Bank(customer_name, account_type)
-    print(a.__dict__)
+    return customer_name, account_type  # Returning the values in the form of a tuple
 
 
 if __name__ == '__main__':
-    accept_input()
+    print("\t\t\tWELCOME", end=" ")
+    try:
+        file = open(r"\.bank_details.txt")
+    except FileNotFoundError:  # If the program finds no existing file in the working directory and prompts the user.
+        with sys.stderr as print_error:
+            print_error.write("\nYou currently have no existing account.")
+            print_error.flush()
+        print("Do you want to create a bank account?")
+        user_choice = int(input("Enter 1 for yes and any number to end the program: "))
+        if user_choice == 1:
+            acc_type, name = accept_input()  # Unpacking the tuple
+        else:
+            print("Have a great day!!!")
