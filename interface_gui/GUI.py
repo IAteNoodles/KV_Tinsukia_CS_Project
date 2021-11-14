6ea8594090d7863e0e4cc2103cff7881c2c0b43b
IAteNoodles <thisabhijithere@protonmail.com>
2021-11-14 16:52:02 +0530

    Test

--- interface_gui/GUI.py
+++ interface_gui/GUI.py
@@ -1,7 +1,6 @@
 from tkinter import *
-import sys
-sys.path.append('/home/groot/KV_Tinsukia_CS_Project/back_end_banking_system/')
-import Bank
+from back_end_banking_system import *
+
 
 class GUI:
     def __init__(self, name="Name of the bank", geometry="720x480"):
@@ -10,35 +9,40 @@ class GUI:
         self.__ROOT.geometry(geometry)
 
     def start_up(self):
-        userFrame = LabelFrame(self.__ROOT,text="User", fg="Green")
-        userFrame.pack(expand = True, fill = 'both')
+        userFrame = LabelFrame(self.__ROOT, text="User", fg="Green")
+        userFrame.pack(expand=True, fill='both')
+
         def login_user():
-            
             login_window = Toplevel(self.__ROOT)
             login_window.title("Account Login Window")
             login_window.geometry("640x400")
-            
-            Label(login_window, text="Unique ID: ").pack(anchor = CENTER)
+
+            Label(login_window, text="Unique ID: ").pack(anchor=CENTER)
             unique_id = Entry(login_window)
-            unique_id.pack(anchor = CENTER)
+            unique_id.pack(anchor=CENTER)
             unique_id = unique_id.get()
-            
-            Label(login_window, text="Password: ").pack(anchor = CENTER)
+
+            Label(login_window, text="Password: ").pack(anchor=CENTER)
             password = Entry(login_window)
-            password.pack(anchor = CENTER)
+            password.pack(anchor=CENTER)
             password = password.get()
-            
+
             def check_credentials():
                 pass
-            Button(login_window, text="Confirm", command = check_credentials).pack(anchor = CENTER)
-            
-        Button(userFrame, text="Login", command = login_user).pack(anchor = CENTER)
+
+            Button(login_window, text="Confirm", command=check_credentials).pack(anchor=CENTER)
+
+        Button(userFrame, text="Login", command=login_user).pack(anchor=CENTER)
+
         def create_account():
             pass
-        Button(userFrame,text="Create an new account", command = create_account).pack(anchor = CENTER)
-        adminFrame = LabelFrame(self.__ROOT,text="Admin", fg="Blue")
-        adminFrame.pack(expand = True, fill = 'both')
+
+        Button(userFrame, text="Create an new account", command=create_account).pack(anchor=CENTER)
+        adminFrame = LabelFrame(self.__ROOT, text="Admin", fg="Blue")
+        adminFrame.pack(expand=True, fill='both')
         helpFrame = LabelFrame(self.__ROOT, text="Help", fg="Red")
-        helpFrame.pack(expand = True, fill = 'both')
+        helpFrame.pack(expand=True, fill='both')
         self.__ROOT.mainloop()
+
+
 GUI().start_up()
